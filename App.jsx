f57ef2899f9c29b87e30e1bd1d4f41dc94593765
Philip Thomsen <philip-th@live.dk>
2023-05-02 09:38:28 +0200

    update

---
+++ App.jsx
@@ -0,0 +1,57 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Pedometer } from 'expo-sensors';
+import { DeviceMotion } from 'expo-sensors';
+import { useEffect, useState } from 'react';
+
+export default function App() {
+  const [isPedometerAvailable, setIsPedometerAvailable] = useState('checking');
+  const [pastStepCount, setPastStepCount] = useState(0);
+  const [currentStepCount, setCurrentStepCount] = useState(0);
+console.log(DeviceMotion);
+  const subscribe = async () => {
+    const isAvailable = await Pedometer.isAvailableAsync();
+    setIsPedometerAvailable(String(isAvailable));
+
+    if (isAvailable) {
+      const end = new Date();
+      const start = new Date();
+      start.setDate(end.getDate() - 1);
+
+      const pastStepCountResult = await Pedometer.getStepCountAsync(start, end);
+      if (pastStepCountResult) {
+        setPastStepCount(pastStepCountResult.steps);
+      }
+
+      return Pedometer.watchStepCount(result => {
+        setCurrentStepCount(result.steps);
+      });
+    }
+  };
+
+  useEffect(() => {
+    const subscription = subscribe();
+    return () => subscription && subscription.remove();
+  }, []);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Dine skridt: {currentStepCount}</Text>
+      <StatusBar style="auto" />
+      
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "green",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: "white",
+    fontSize: "3rem"
+  }
+});
